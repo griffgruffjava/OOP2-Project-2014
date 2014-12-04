31f81f905f1e33007392578c57fb7c25eb9da19f
unknown <t00175569@1XX782BCBAC2836.students.ittralee.local>
2014-12-04 16:23:24 +0000

    working on gui interface, still not working

--- QuizDriver3.java
+++ QuizDriver3.java
@@ -4,11 +4,11 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
-public class QuizDriver2 extends JFrame implements ActionListener
+public class QuizDriver3 extends JFrame implements ActionListener
 {
 	JMenu playMenu;
 	JMenu adminMenu;
-	static QuizDriver2 runBox;
+	static QuizDriver3 runBox;
 	int i=0;
 	boolean isCorrect,addMore=true;
 	String answer="start", question, isCorrectString, addMoreString, choice, answerKey;
@@ -22,12 +22,12 @@ public class QuizDriver2 extends JFrame implements ActionListener
 	
 	public static void main(String[] args)
 	{	
-		runBox = new QuizDriver2();
+		runBox = new QuizDriver3();
 		runBox.setVisible(true);
 	
 	}//end main	
 		
-	public QuizDriver2()
+	public QuizDriver3()
 	{
 		
 		setTitle("Networking Quiz");
@@ -48,6 +48,8 @@ public class QuizDriver2 extends JFrame implements ActionListener
 		
 	}//end constructor	
 	
+	
+	//my event handler method for menu items
 	public void actionPerformed(ActionEvent event)
 	{
 		String menuName;
@@ -80,7 +82,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
       	 	System.out.print("Not able to save the file:\n"+
       	 	"Check the console printout for clues to why ");
       	 	f.printStackTrace();
-      	 }// catch
+      	 }// catch 
 			
 		}
 		else if(menuName.equals("Edit Question"))
@@ -134,96 +136,24 @@ public class QuizDriver2 extends JFrame implements ActionListener
 	}// end createAdminMenu method 
 	
 	
-	private void addQuestion()
-	{
-		JLabel question,a,b,c,d,e,f;
-		JTextField txtQues,txtA,txtB,txtC,txtD,txtE,txtF;
-		JButton btnAdd;
-		JPanel p1;
-		GridLayout grid;
-		//QuizDriver runBox2 = new QuizDriver();
-		
-		//runBox2.setVisible(true);
-		
-		question = new JLabel("Enter Question:");
-		a= new JLabel("Option A");
-		b= new JLabel("Option B");
-		c= new JLabel("Option C");
-		d= new JLabel("Option D");
-		e= new JLabel("Option E");
-		f= new JLabel("Option F");
-		
-		txtQues = new JTextField(100);
-		txtA = new JTextField(50);
-		txtB = new JTextField(50);
-		txtC = new JTextField(50);
-		txtD = new JTextField(50);
-		txtE = new JTextField(50);
-		txtF = new JTextField(50);
-		
-		btnAdd = new JButton("Add Question");  //http://www.codingmagazine.net/2013/01/how-to-create-simple-form-using-java-swing.html
-		
-		p1 = new JPanel();
-		grid = new GridLayout(4,2);
-		FlowLayout f1 = new FlowLayout();
-		
-		p1.setLayout(grid);
-		
-		p1.add(question);
-		p1.add(txtQues);
-		
-		p1.add(a);
-		p1.add(txtA);
-		
-		p1.add(b);
-		p1.add(txtB);
-		
-		p1.add(c);
-		p1.add(txtC);
-		
-		p1.add(d);
-		p1.add(txtD);
-		
-		p1.add(e);
-		p1.add(txtE);
-		
-		p1.add(f);
-		p1.add(txtF);
-		
-		p1.add(btnAdd);
-		
-	//	cPane.add(f1);
-      runBox.add(p1);
-		
-	//	return p1;
-		
-		
-		
-	}//end addQuestion method
+
 	
 	private void addNewQuestions()throws IOException 
 	{
-      //	File inFile = new File("allQuestions");
-      	
-      //	FileInputStream inStream = new FileInputStream(inFile);
-      //	FileOutputStream outStream = new FileOutputStream(outFile);
-     // 	DataInputStream inDataStream = new DataInputStream(inStream);
-     // 	DataOutputStream outDataStream = new DataOutputStream(outStream);
      	
      	try{
       	  ObjectInputStream is;
-      	  is = new ObjectInputStream(new FileInputStream ("allQuestions.dat"));
+      	  is = new ObjectInputStream(new FileInputStream ("allQuestion.dat"));
          	questions  = (ArrayList<Question>) is.readObject();
       	  is.close(); 
       	}
       	catch(Exception e){
-      		JOptionPane.showMessageDialog(null,"open didn't work");
-      		e.printStackTrace();
+      	//	this is left empty to prevent exceptions being displayed in output
       	}
      	
 	
 		addMore=true;
-		System.out.println("made it here A");
+	
 		//to populate questions 
 		while(addMore==true)
 		{
@@ -248,7 +178,6 @@ public class QuizDriver2 extends JFrame implements ActionListener
 					
 					
 					ans = new Answer(answer,isCorrect);
-				//	System.out.println(ans.toString());  -tests that Answer object is made
 					answers.add(ans);		
 				}
 				
@@ -260,10 +189,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
 			answer="temp";
 			
 			q1 = new Question(question,answers);
-			System.out.println(q1.toStringFull());
-		/*	q1 = new Question();
-			q1.setQuestion(question);
-			q1.setAns(answers);	*/
+		//	System.out.println(q1.toStringFull()); //to test that questions are being made
 			
 			addMoreString=JOptionPane.showInputDialog("Do you want to add another question (y/n)?");
 			if(addMoreString.charAt(0)=='n'||addMoreString.charAt(0)=='N')
@@ -280,7 +206,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
 			questions.add(q1);
 			
 			try{
-      	 		save();
+      	 		save();//method to save questions ArrayList to dat file
       	 		System.out.println("Questions saved successfully");
       	 	} // try
       	 	catch (IOException f){
@@ -289,15 +215,14 @@ public class QuizDriver2 extends JFrame implements ActionListener
       	 		f.printStackTrace();
       	 	}// catch
 			
-		/*	FileOutputStream outFile = new FileOutputStream("allQuestions");
-			ObjectOutputStream os = new ObjectOutputStream(outFile);
-			os.writeObject(questions);
-			os.close(); */
+
 				
 		}//end of outer loop for making whole questions
 		
 	}//end of addNewQuestions method	
 	
+	
+	// start of basic quiz method 
 	private void takeQuiz()
 	{
 			
@@ -322,7 +247,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
 	}//end takeQuiz() method
 	
 	
-	
+	//this method will tabulate the score for takeQuiz method
 	public static double scoreAnswers(String key,String choices)
 	{
 		int outOf=key.length();
@@ -353,10 +278,12 @@ public class QuizDriver2 extends JFrame implements ActionListener
 				
 	}//end scoreAnswers() method
 	
+	
+	//method to write questions ArryList to dat file
 	public void save() throws IOException
     {
       	ObjectOutputStream os;
-      	os = new ObjectOutputStream(new FileOutputStream ("allQuestions.dat"));
+      	os = new ObjectOutputStream(new FileOutputStream ("allQuestion.dat"));
       	os.writeObject(questions);
       	os.close();
     }
--- QuizDriver4.java
+++ QuizDriver4.java
@@ -1,15 +1,21 @@
+/*this will attempt to put JOP input/outputs onto gui jframe */
+
+
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 
-public class QuizDriver2 extends JFrame implements ActionListener
+public class QuizDriver4 extends JFrame implements ActionListener
 {
 	JMenu playMenu;
 	JMenu adminMenu;
-	static QuizDriver2 runBox;
+	Container cPane;
+	static QuizDriver4 runBox;
 	int i=0;
+	JLabel questionLabel,aLabel,bLabel,cLabel,dLabel,eLabel,fLabel;
+	JTextField questionField,aField,bField,cField,dField,eField,fField;
 	boolean isCorrect,addMore=true;
 	String answer="start", question, isCorrectString, addMoreString, choice, answerKey;
 	ArrayList<Answer> answers;
@@ -22,12 +28,12 @@ public class QuizDriver2 extends JFrame implements ActionListener
 	
 	public static void main(String[] args)
 	{	
-		runBox = new QuizDriver2();
+		runBox = new QuizDriver4();
 		runBox.setVisible(true);
 	
 	}//end main	
 		
-	public QuizDriver2()
+	public QuizDriver4()
 	{
 		
 		setTitle("Networking Quiz");
@@ -35,8 +41,8 @@ public class QuizDriver2 extends JFrame implements ActionListener
 		setLocation(300,500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-	/*	cPane = getContentPane();
-		cPane.setLayout(new FlowLayout());*/
+		cPane = getContentPane();
+		cPane.setLayout(null);
 		
 		createPlayMenu();
 		createAdminMenu();
@@ -46,8 +52,18 @@ public class QuizDriver2 extends JFrame implements ActionListener
 		menuBar.add(playMenu);
 		menuBar.add(adminMenu);
 		
+		questionLabel = new JLabel("This is a test");
+		questionField = new JTextField();
+	//	questionField.setBounds(110,130,80,40);
+	//	runBox.add(questionLabel);
+	//	runBox.add(questionField);
+		cPane.add(questionLabel);
+		cPane.add(questionField);
+		
 	}//end constructor	
 	
+	
+	//my event handler method for menu items
 	public void actionPerformed(ActionEvent event)
 	{
 		String menuName;
@@ -60,7 +76,9 @@ public class QuizDriver2 extends JFrame implements ActionListener
 		}
 		else if(menuName.equals("25 Questions"))
 		{
+			
 			JOptionPane.showMessageDialog(null,"you picked 25 quesitons","test response",JOptionPane.PLAIN_MESSAGE);
+			testAddingToFrame();
 		}
 		else if(menuName.equals("50 Questions"))
 		{
@@ -73,14 +91,14 @@ public class QuizDriver2 extends JFrame implements ActionListener
 			System.out.println("made it here c");
 		//	addNewQuestions();
 		try{
-      	 	addNewQuestions();
+      	 	addFrameQuestions();
       	 	System.out.print("made it to try");
       	 } // try
       	 catch (IOException f){
       	 	System.out.print("Not able to save the file:\n"+
       	 	"Check the console printout for clues to why ");
       	 	f.printStackTrace();
-      	 }// catch
+      	 }// catch 
 			
 		}
 		else if(menuName.equals("Edit Question"))
@@ -134,96 +152,24 @@ public class QuizDriver2 extends JFrame implements ActionListener
 	}// end createAdminMenu method 
 	
 	
-	private void addQuestion()
-	{
-		JLabel question,a,b,c,d,e,f;
-		JTextField txtQues,txtA,txtB,txtC,txtD,txtE,txtF;
-		JButton btnAdd;
-		JPanel p1;
-		GridLayout grid;
-		//QuizDriver runBox2 = new QuizDriver();
-		
-		//runBox2.setVisible(true);
-		
-		question = new JLabel("Enter Question:");
-		a= new JLabel("Option A");
-		b= new JLabel("Option B");
-		c= new JLabel("Option C");
-		d= new JLabel("Option D");
-		e= new JLabel("Option E");
-		f= new JLabel("Option F");
-		
-		txtQues = new JTextField(100);
-		txtA = new JTextField(50);
-		txtB = new JTextField(50);
-		txtC = new JTextField(50);
-		txtD = new JTextField(50);
-		txtE = new JTextField(50);
-		txtF = new JTextField(50);
-		
-		btnAdd = new JButton("Add Question");  //http://www.codingmagazine.net/2013/01/how-to-create-simple-form-using-java-swing.html
-		
-		p1 = new JPanel();
-		grid = new GridLayout(4,2);
-		FlowLayout f1 = new FlowLayout();
-		
-		p1.setLayout(grid);
-		
-		p1.add(question);
-		p1.add(txtQues);
-		
-		p1.add(a);
-		p1.add(txtA);
-		
-		p1.add(b);
-		p1.add(txtB);
-		
-		p1.add(c);
-		p1.add(txtC);
-		
-		p1.add(d);
-		p1.add(txtD);
-		
-		p1.add(e);
-		p1.add(txtE);
-		
-		p1.add(f);
-		p1.add(txtF);
-		
-		p1.add(btnAdd);
-		
-	//	cPane.add(f1);
-      runBox.add(p1);
-		
-	//	return p1;
-		
-		
-		
-	}//end addQuestion method
+
 	
 	private void addNewQuestions()throws IOException 
 	{
-      //	File inFile = new File("allQuestions");
-      	
-      //	FileInputStream inStream = new FileInputStream(inFile);
-      //	FileOutputStream outStream = new FileOutputStream(outFile);
-     // 	DataInputStream inDataStream = new DataInputStream(inStream);
-     // 	DataOutputStream outDataStream = new DataOutputStream(outStream);
      	
      	try{
       	  ObjectInputStream is;
-      	  is = new ObjectInputStream(new FileInputStream ("allQuestions.dat"));
+      	  is = new ObjectInputStream(new FileInputStream ("allQuestion.dat"));
          	questions  = (ArrayList<Question>) is.readObject();
       	  is.close(); 
       	}
       	catch(Exception e){
-      		JOptionPane.showMessageDialog(null,"open didn't work");
-      		e.printStackTrace();
+      	//	this is left empty to prevent exceptions being displayed in output
       	}
      	
 	
 		addMore=true;
-		System.out.println("made it here A");
+	
 		//to populate questions 
 		while(addMore==true)
 		{
@@ -248,7 +194,6 @@ public class QuizDriver2 extends JFrame implements ActionListener
 					
 					
 					ans = new Answer(answer,isCorrect);
-				//	System.out.println(ans.toString());  -tests that Answer object is made
 					answers.add(ans);		
 				}
 				
@@ -260,10 +205,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
 			answer="temp";
 			
 			q1 = new Question(question,answers);
-			System.out.println(q1.toStringFull());
-		/*	q1 = new Question();
-			q1.setQuestion(question);
-			q1.setAns(answers);	*/
+		//	System.out.println(q1.toStringFull()); //to test that questions are being made
 			
 			addMoreString=JOptionPane.showInputDialog("Do you want to add another question (y/n)?");
 			if(addMoreString.charAt(0)=='n'||addMoreString.charAt(0)=='N')
@@ -280,7 +222,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
 			questions.add(q1);
 			
 			try{
-      	 		save();
+      	 		save();//method to save questions ArrayList to dat file
       	 		System.out.println("Questions saved successfully");
       	 	} // try
       	 	catch (IOException f){
@@ -288,16 +230,116 @@ public class QuizDriver2 extends JFrame implements ActionListener
       	 		"Check the console printout for clues to why ");
       	 		f.printStackTrace();
       	 	}// catch
+      	 	
+      	 	
 			
-		/*	FileOutputStream outFile = new FileOutputStream("allQuestions");
-			ObjectOutputStream os = new ObjectOutputStream(outFile);
-			os.writeObject(questions);
-			os.close(); */
+
 				
 		}//end of outer loop for making whole questions
 		
 	}//end of addNewQuestions method	
 	
+	
+	private void testAddingToFrame()
+	{
+		questionLabel = new JLabel("This is a test");
+		questionField = new JTextField();
+	//	questionField.setBounds(110,130,80,40);
+	//	runBox.add(questionLabel);
+	//	runBox.add(questionField);
+		cPane.add(questionLabel);
+		cPane.add(questionField);
+		
+	}
+	
+	private void addFrameQuestions()throws IOException 
+	{
+     	
+     	try{
+      	  ObjectInputStream is;
+      	  is = new ObjectInputStream(new FileInputStream ("allQuestion.dat"));
+         	questions  = (ArrayList<Question>) is.readObject();
+      	  is.close(); 
+      	}
+      	catch(Exception e){
+      	//	this is left empty to prevent exceptions being displayed in output
+      	}
+     	
+		
+		
+		
+		
+		addMore=true;
+	
+		//to populate questions 
+		while(addMore==true)
+		{
+			
+			System.out.println("made it here B");
+			answers = new ArrayList();
+			question=JOptionPane.showInputDialog("What is the question?");
+		
+		
+			while(i<6 && !answer.equals(""))
+			{
+				answer=JOptionPane.showInputDialog("Add an optional answer(hit enter to add no more options)");
+				
+				if(!answer.equals(""))
+				{
+					isCorrectString=JOptionPane.showInputDialog("Is this option true or false");
+					
+					if(isCorrectString.charAt(0)=='f'||isCorrectString.charAt(0)=='F')
+						isCorrect=false;
+					else
+						isCorrect=true;	
+					
+					
+					ans = new Answer(answer,isCorrect);
+					answers.add(ans);		
+				}
+				
+				i++;	
+			}//end of inner while loop for adding answers
+			
+			//reset counter and answer
+			i=0;
+			answer="temp";
+			
+			q1 = new Question(question,answers);
+		//	System.out.println(q1.toStringFull()); //to test that questions are being made
+			
+			addMoreString=JOptionPane.showInputDialog("Do you want to add another question (y/n)?");
+			if(addMoreString.charAt(0)=='n'||addMoreString.charAt(0)=='N')
+			{
+				addMore=false;
+				System.out.println("you said stop " + addMoreString.charAt(0));
+			}
+			else
+			{
+				addMore=true;
+				System.out.println("you said add more " + addMoreString.charAt(0) );	
+			}
+			
+			questions.add(q1);
+			
+			try{
+      	 		save();//method to save questions ArrayList to dat file
+      	 		System.out.println("Questions saved successfully");
+      	 	} // try
+      	 	catch (IOException f){
+      	 		System.out.println("Not able to save the file:\n"+
+      	 		"Check the console printout for clues to why ");
+      	 		f.printStackTrace();
+      	 	}// catch
+      	 	
+      	 	
+			
+
+				
+		}//end of outer loop for making whole questions
+		
+	}//end of addNewQuestions method
+	// start of basic quiz method 
 	private void takeQuiz()
 	{
 			
@@ -322,7 +364,7 @@ public class QuizDriver2 extends JFrame implements ActionListener
 	}//end takeQuiz() method
 	
 	
-	
+	//this method will tabulate the score for takeQuiz method
 	public static double scoreAnswers(String key,String choices)
 	{
 		int outOf=key.length();
@@ -353,10 +395,12 @@ public class QuizDriver2 extends JFrame implements ActionListener
 				
 	}//end scoreAnswers() method
 	
+	
+	//method to write questions ArryList to dat file
 	public void save() throws IOException
     {
       	ObjectOutputStream os;
-      	os = new ObjectOutputStream(new FileOutputStream ("allQuestions.dat"));
+      	os = new ObjectOutputStream(new FileOutputStream ("allQuestion.dat"));
       	os.writeObject(questions);
       	os.close();
     }
